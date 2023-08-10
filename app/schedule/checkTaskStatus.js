4df520ba4a6e4520e82cfc3e2677d00a0b2861d8
xjs <13165310+milkyxway@user.noreply.gitee.com>
2023-08-10 10:46:13 +0800

    fix: fix code

--- app/schedule/checkTaskStatus.js
+++ app/schedule/checkTaskStatus.js
@@ -18,31 +18,38 @@ class logTime extends Subscription {
 		});
 		const delayTask = list.filter((i) => i.finishTime < dayjs(Date().now));
 		if (delayTask.length) {
+			let delayTimes = 1;
 			delayTask.map(async (i) => {
+				if (i.delayTimes !== null) {
+					delayTimes = i.delayTimes + 1;
+				}
 				await this.app.mysql.update(
 					"subtask_list",
-					{ status: 5 },
+					{ status: 5, delayTimes, updateTime: new Date() },
 					{ where: { subtaskId: i.subtaskId } }
 				);
 				await this.app.mysql.update(
 					"task_list",
-					{ status: 5 },
+					{ status: 5, updateTime: new Date() },
 					{ where: { taskId: i.parentId } }
 				);
 			});
 		}
 
-		const result = await this.app.mysql.select("task_list", {
-			where: { status: 3 },
-		});
+		const sql = `select * from task_list where status = 3 and resolveType = null`;
+		const result = await this.app.mysql.query(sql);
 		const delayMainTask = result.filter(
 			(i) => i.finishTime < dayjs(Date().now)
 		);
 		if (delayMainTask.length) {
+			let delayTimes = 1;
 			delayMainTask.map(async (i) => {
+				if (delayTimes !== null) {
+					delayTimes = i.delayTimes + 1;
+				}
 				await this.app.mysql.update(
 					"task_list",
-					{ status: 5 },
+					{ status: 5, delayTimes, updateTime: new Date() },
 					{ where: { taskId: i.taskId } }
 				);
 			});
